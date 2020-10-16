8ad39dec09cede17e86bcc22d1a834a405e33e3d
Adhi Baskoro <abas0012@student.monash.edu>
2020-10-17 08:27:31 +1100

    Updates

---
+++ precache-manifest.8364c3bcf177914a1fd8bb7ea050c1d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "945e8f75ac2382da9c2405126713f676",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "e9ffc485fd51443c0e4f",
+    "url": "/myapp/static/css/main.c7ab3b04.chunk.css"
+  },
+  {
+    "revision": "4cfc160bb3d13546e2db",
+    "url": "/myapp/static/js/2.c5010d1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapp/static/js/2.c5010d1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9ffc485fd51443c0e4f",
+    "url": "/myapp/static/js/main.06c2eb1f.chunk.js"
+  },
+  {
+    "revision": "dfc084a7ee4f2eebf7cd",
+    "url": "/myapp/static/js/runtime-main.5f0af923.js"
+  }
+]);
